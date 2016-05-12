067865a2a9e9c8f8e06d9c29a34fc44df93dc73f
Alejandro Mendez <amendez23@gmail.com>
2016-05-12 10:06:45 +0100

    Implement caption parsing

--- tests/parser.py
+++ tests/parser.py
@@ -2,7 +2,7 @@ import os
 import unittest
 
 from webvtt import WebVTTParser
-from webvtt.exceptions import MalformedFileError
+from webvtt.exceptions import MalformedFileError, MalformedCaptionError
 
 SUBTITLES_DIR = os.path.dirname(os.path.dirname(__file__))
 SUBTITLES_DIR = os.path.join(SUBTITLES_DIR, 'tests/subtitles')
@@ -14,7 +14,7 @@ class WebVTTParserTestCase(unittest.TestCase):
         self.parser = WebVTTParser()
 
     def test_parser_valid_webvtt(self):
-        self.assertTrue(self.parser.read(os.path.join(SUBTITLES_DIR, 'sample.vtt')).is_valid())
+        self.assertTrue(self.parser.read(os.path.join(SUBTITLES_DIR, 'sample.vtt')).captions)
 
     def test_parser_invalid_webvtt(self):
         self.assertRaises(
@@ -27,3 +27,26 @@ class WebVTTParserTestCase(unittest.TestCase):
             MalformedFileError,
             self.parser.read,
             os.path.join(SUBTITLES_DIR, 'empty.vtt'))
+
+    def test_parser_get_captions(self):
+        self.assertEqual(
+            len(self.parser.read(os.path.join(SUBTITLES_DIR, 'sample.vtt')).captions), 7)
+
+    def test_parser_invalid_timeframe_line(self):
+        self.assertRaises(
+            MalformedCaptionError,
+            self.parser.read,
+            os.path.join(SUBTITLES_DIR, 'invalid_timeframe.vtt'))
+
+    def test_parser_get_caption_data(self):
+        self.parser.read(os.path.join(SUBTITLES_DIR, 'one_caption.vtt'))
+        self.assertEqual(self.parser.captions[0].start, '00:00:00.500')
+        self.assertEqual(self.parser.captions[0].end, '00:00:07.000')
+        self.assertEqual(self.parser.captions[0].lines[0], 'Caption text #1')
+        self.assertEqual(len(self.parser.captions[0].lines), 1)
+
+    def test_caption_without_timeframe(self):
+        self.assertRaises(
+            MalformedCaptionError,
+            self.parser.read,
+            os.path.join(SUBTITLES_DIR, 'missing_timeframe.vtt'))
--- webvtt/parser.py
+++ webvtt/parser.py
@@ -1,20 +1,56 @@
-from .exceptions import MalformedFileError
+import re
+
+from .generic import Caption
+from .exceptions import MalformedFileError, MalformedCaptionError
+
+TIMEFRAME_LINE_PATTERN = re.compile('\s*(\d+:\d{2}:\d{2}.\d{3})\s*-->\s*(\d+:\d{2}:\d{2}.\d{3})')
 
 
 class WebVTTParser:
 
-    def _parse(self, content):
-        self.content = content
+    def _parse_timeframe_line(self, line, line_number):
+        """Parse timeframe line and return start and end timestamps"""
+        res = re.match(TIMEFRAME_LINE_PATTERN, line)
+        if not res:
+            raise MalformedCaptionError('Invalid time format in line {}'.format(line_number))
+
+        return res.group(1), res.group(2)
+
+    def _parse(self):
+        c = None
+        for index, line in enumerate(self.lines[1:]):
+            line = line.strip()
+            if '-->' in line:
+                start, end = self._parse_timeframe_line(line, index)
+                c = Caption(start, end)
+            elif len(line) > 0:
+                if c is None:
+                    raise MalformedCaptionError('Caption missing timeframe in line {}'.format(index + 1))
+                else:
+                    c.add_line(line)
+            else:
+                if c is None:
+                    continue
+                if not c.lines:
+                    raise MalformedCaptionError('Caption missing text in line {}'.format(index + 1))
+
+                self.captions.append(c)
+                c = None
+
+        if c is not None and c.lines:
+            self.captions.append(c)
 
     def read(self, file):
+        self.captions = []
+
         with open(file, encoding='utf-8') as f:
-            self._parse(f.readlines())
-        if len(self.content) == 0:
+            self.lines = f.readlines()
+
+        if len(self.lines) == 0:
             raise MalformedFileError('The file is empty')
-        if not self.is_valid():
+        if 'WEBVTT' not in self.lines[0]:
             raise MalformedFileError('The file does not have a valid format')
 
-        return self
+        self._parse()
 
-    def is_valid(self):
-        return 'WEBVTT' in self.content[0]
+        return self
