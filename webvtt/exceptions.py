4530325e460d38086201573d85a9ae95fc877a4c
Alejandro Mendez <amendez23@gmail.com>
2016-05-12 09:53:27 +0100

    Add new exception for malformed captions

--- webvtt/exceptions.py
+++ webvtt/exceptions.py
@@ -2,3 +2,7 @@
 
 class MalformedFileError(Exception):
     """Error raised when the file is not well formatted"""
+
+
+class MalformedCaptionError(Exception):
+    """Error raised when a caption is not well formatted"""
