5420dc57d6673b60b4151934ad344cd29067ec3d
Alejandro Mendez <amendez23@gmail.com>
2016-05-13 14:17:35 +0100

    First implementation of the segmenter

---
+++ tests/segmenter.py
@@ -0,0 +1,72 @@
+import os
+import unittest
+from shutil import rmtree
+
+from webvtt import WebVTTSegmenter, Caption
+from webvtt.exceptions import InvalidCaptionsError
+from webvtt.parser import WebVTTParser
+
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+SUBTITLES_DIR = os.path.join(BASE_DIR, 'tests/subtitles')
+OUTPUT_DIR = os.path.join(BASE_DIR, 'tests/output')
+
+
+class WebVTTSegmenterTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.segmenter = WebVTTSegmenter()
+
+    def tearDown(self):
+        if os.path.exists(OUTPUT_DIR):
+            rmtree(OUTPUT_DIR)
+
+    def _parse_captions(self, filename):
+        self.parser = WebVTTParser().read(os.path.join(SUBTITLES_DIR, filename))
+
+    def test_invalid_captions(self):
+        self.assertRaises(
+            InvalidCaptionsError,
+            self.segmenter.segment,
+            'text'
+        )
+
+    def test_single_invalid_caption(self):
+        self.assertRaises(
+            InvalidCaptionsError,
+            self.segmenter.segment,
+            [Caption(), Caption(), 'text', Caption()]
+        )
+
+    def test_total_segments(self):
+        # segment with default 10 seconds
+        self._parse_captions('sample.vtt')
+        self.segmenter.segment(self.parser.captions, OUTPUT_DIR)
+        self.assertEqual(self.segmenter.total_segments, 7)
+
+        # segment with custom 30 seconds
+        self._parse_captions('sample.vtt')
+        self.segmenter.segment(self.parser.captions, OUTPUT_DIR, 30)
+        self.assertEqual(self.segmenter.total_segments, 3)
+
+    def test_output_folder_is_created(self):
+        self.assertFalse(os.path.exists(OUTPUT_DIR))
+        self._parse_captions('sample.vtt')
+        self.segmenter.segment(self.parser.captions, OUTPUT_DIR)
+        self.assertTrue(os.path.exists(OUTPUT_DIR))
+
+    def test_segmentation_files_exist(self):
+        self._parse_captions('sample.vtt')
+        self.segmenter.segment(self.parser.captions, OUTPUT_DIR)
+        for i in range(7):
+            self.assertTrue(
+                os.path.exists(os.path.join(OUTPUT_DIR, 'fileSequence{}.webvtt'.format(i)))
+            )
+        self.assertTrue(os.path.exists(os.path.join(OUTPUT_DIR, 'prog_index.m3u8')))
+
+    def test_manifest_content(self):
+        self._parse_captions('sample.vtt')
+        self.segmenter.segment(self.parser.captions, OUTPUT_DIR, 10)
+        with open(os.path.join(OUTPUT_DIR, 'prog_index.m3u8'), 'r', encoding='utf-8') as f:
+            lines = [line.rstrip() for line in f.readlines()]
+            self.assertEqual(lines[0], '#EXTM3U')
+            self.assertEqual(lines[1], '#EXT-X-TARGETDURATION:')
---
+++ webvtt/segmenter.py
@@ -0,0 +1,54 @@
+import os
+from math import ceil
+
+from .exceptions import InvalidCaptionsError
+from .generic import Caption
+
+
+class WebVTTSegmenter(object):
+    MPEGTS = 900000
+    SECONDS = 10  # default number of seconds per segment
+
+    def __init__(self):
+        self.total_segments = 0
+        self._output_folder = ''
+        self._seconds = 0
+
+    def _validate_captions(self, captions):
+        """validates that the captions is a list and all the captions are instances of Caption"""
+        if not isinstance(captions, list):
+            return False
+        for c in captions:
+            if not isinstance(c, Caption):
+                return False
+        return True
+
+    def _write_segments(self):
+        for index in range(self.total_segments):
+            current_second = index * self._seconds
+            segment_file = os.path.join(self._output_folder, 'fileSequence{}.webvtt'.format(index))
+
+            with open(segment_file, 'w', encoding='utf-8') as f:
+                pass
+
+    def _write_manifest(self):
+        manifest_file = os.path.join(self._output_folder, 'prog_index.m3u8')
+        with open(manifest_file, 'w', encoding='utf-8') as f:
+            f.write('#EXTM3U\n')
+            f.write('#EXT-X-TARGETDURATION:\n')
+
+    def segment(self, captions, output='', seconds=SECONDS):
+        if not self._validate_captions(captions):
+            raise InvalidCaptionsError('The captions provided are invalid')
+
+        self.total_segments = int(ceil(captions[-1].end_in_seconds / seconds))
+        self._output_folder = output
+        self._seconds = seconds
+
+        output_folder = os.path.join(os.getcwd(), output)
+        if not os.path.exists(output_folder):
+            os.makedirs(output_folder)
+
+        self._write_segments()
+        self._write_manifest()
+
