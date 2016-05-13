503d3f8a42c42851596d5b8d566ae9c20a1a17ac
Alejandro Mendez <amendez23@gmail.com>
2016-05-13 14:16:25 +0100

    Inlcude properties to get start and end times in seconds

--- webvtt/generic.py
+++ webvtt/generic.py
@@ -1,11 +1,22 @@
-from datetime import datetime
+from datetime import time
 
 
 class Caption(object):
-    def __init__(self, start=datetime.now(), end=datetime.now(), lines=None):
+    def __init__(self, start=time(0, 0, 0), end=time(0, 0, 0), lines=None):
         self.start = start
         self.end = end
         self.lines = lines or []
 
     def add_line(self, line):
         self.lines.append(line)
+
+    def _time_in_seconds(self, time):
+        return time.second + time.minute * 60 + time.hour * 60 * 60
+
+    @property
+    def start_in_seconds(self):
+        return self._time_in_seconds(self.start)
+
+    @property
+    def end_in_seconds(self):
+        return self._time_in_seconds(self.end)
