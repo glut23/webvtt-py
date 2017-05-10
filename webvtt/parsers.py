a9314b939e06cd31019fe7cb224350a9af98a915
Vemund Kvam <vemund.kvam@gmail.com>
2017-05-10 14:26:30 +0200

    Update parsers.py

--- webvtt/parsers.py
+++ webvtt/parsers.py
@@ -13,7 +13,7 @@ class TextBasedParser(GenericParser):
     TIMEFRAME_LINE_PATTERN = ''
 
     def _read_content(self, file):
-        with open(file, encoding='utf-8') as f:
+        with open(file) as f:
             lines = [line.rstrip() for line in f.readlines()]
 
         if not lines:
