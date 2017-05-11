dfaca86eb491d019e81cfe3519bb8e889c8a142a
Vemund Kvam <vemund.kvam@get.no>
2017-05-11 15:30:02 +0200

    handle vtt timestamps with hours

--- webvtt/generic.py
+++ webvtt/generic.py
@@ -2,14 +2,15 @@ import re
 
 from webvtt.exceptions import MalformedCaptionError
 
-TIMESTAMP_PATTERN = re.compile('(\d+):(\d{2})[.,](\d{3})')
+TIMESTAMP_PATTERN = re.compile('(\d+):(\d{2}):(\d{2})[.,](\d{3})')
+TIMESTAMP_PATTERN_SHORT = re.compile('(\d+):(\d{2})[.,](\d{3})')
 
 
 class Caption(object):
     """
     Represents a caption.
     """
-    def __init__(self, start='00:00.000', end='00:00.000', text=None):
+    def __init__(self, start='00:00:00.000', end='00:00:00.000', text=None):
         self.start = start
         self.end = end
 
@@ -22,23 +23,33 @@ class Caption(object):
     def add_line(self, line):
         self.lines.append(line)
 
-    def _to_seconds(self, minutes, seconds, milliseconds):
-        return minutes * 60 + seconds + float(milliseconds) / float(1000)
+    def _to_seconds(self, hours, minutes, seconds, milliseconds):
+        return hours*60*60 + minutes * 60 + seconds + float(milliseconds) / float(1000)
 
     def _parse_timestamp(self, timestamp):
         res = re.match(TIMESTAMP_PATTERN, timestamp)
         if not res:
-            raise MalformedCaptionError('Invalid timestamp: {}'.format(timestamp))
+            res = re.match(TIMESTAMP_PATTERN_SHORT, timestamp)
+            if not res:
+                raise MalformedCaptionError('Invalid timestamp: {}'.format(timestamp))
+            return self._to_seconds(
+                0,
+                int(res.group(1)),  # minutes
+                int(res.group(2)),  # seconds
+                int(res.group(3))  # milliseconds
+            )
         return self._to_seconds(
-            int(res.group(1)),  # minutes
-            int(res.group(2)),  # seconds
-            int(res.group(3))  # milliseconds
+            int(res.group(1)),  # hours
+            int(res.group(2)),  # minutes
+            int(res.group(3)),  # seconds
+            int(res.group(4))  # milliseconds
         )
 
     def _to_timestamp(self, total_seconds):
-        minutes = int(total_seconds / 60)
-        seconds = total_seconds - minutes * 60
-        return '{:02d}:{:06.3f}'.format(int(minutes), seconds)
+        hours = int(total_seconds / 3600)
+        minutes = int(total_seconds / 60) - hours * 60
+        seconds = total_seconds - hours*3600 - minutes * 60
+        return '{:02d}:{:02d}:{:06.3f}'.format(int(hours), int(minutes), seconds)
 
     @property
     def start_in_seconds(self):
