bbe3f16994048a85f65add395515bfcefa1f431f
Alejandro Mendez <amendez23@gmail.com>
2017-09-16 21:31:39 +0100

    Remove Captions class

--- webvtt/main.py
+++ webvtt/main.py
@@ -1,65 +1,8 @@
 import os
-import re
-from enum import IntEnum
 
 from .parsers import WebVTTParser, SRTParser, SBVParser
 from .writers import WebVTTWriter, SRTWriter
 from webvtt.exceptions import MissingFilenameError
-
-
-class Captions(object):
-
-    FORMAT_EXTENSION_PATTERN = re.compile('.+\(\.(.+)\)')
-    OUR_PARSER = None
-
-    def __init__(self, parse_options=None):
-        self._captions = []
-        self._styles = []
-        self.file = ''
-        self.extension = ''
-        self.parse_options = parse_options or {}
-
-    def __len__(self):
-        return len(self._captions)
-
-    def __getitem__(self, index):
-        return self._captions[index]
-
-    def save(self, output=''):
-        """Save the document.
-        If no output is provided the file will be saved in the same location. Otherwise output
-        can determine a target directory or file.
-        """
-        if not output:
-            if not self.file:
-                raise MissingFilenameError
-            # saving an original vtt file will overwrite the file
-            # and for files read from other formats will save as vtt
-            # with the same name and location
-            self.file = os.path.splitext(self.file)[0] + '.' + self.extension
-        else:
-            target = os.path.join(os.getcwd(), output)
-            if os.path.isdir(target):
-                # if an output is provided and it is a directory
-                # the file will be saved in that location with the same name
-                filename = os.path.splitext(os.path.basename(self.file))[0]
-                self.file = os.path.join(target, '{}.{}'.format(filename, self.extension))
-            else:
-                if target[-3:].lower() != self.extension:
-                    target += '.' + self.extension
-                # otherwise the file will be written in the specified location
-                self.file = target
-
-        self.save_format()
-
-    def save_format(self):
-        raise NotImplementedError
-
-
-class OutputFormat(IntEnum):
-    WebVTT = 1
-    SRT = 2
-    SBV = 3
 
 
 class WebVTT(object):
@@ -141,12 +84,12 @@ class WebVTT(object):
     def save_as_srt(self, output=''):
         self.file = self._get_output_file(output, extension='srt')
         with open(self.file, 'w', encoding='utf-8') as f:
-            self.write(f, output_format=OutputFormat.SRT)
+            self.write(f, convert='srt')
 
-    def write(self, f, output_format=OutputFormat.WebVTT):
-        if output_format == OutputFormat.WebVTT:
+    def write(self, f, convert=None):
+        if convert is None:
             WebVTTWriter().write(self._captions, f)
-        elif output_format == OutputFormat.SRT:
+        elif convert == 'srt':
             SRTWriter().write(self._captions, f)
 #        elif output_format == OutputFormat.SBV:
 #            SBVWriter().write(self._captions, f)
