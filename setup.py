7d84c016ad7ce0a9fc2dc0c3f57b23794465b4f8
Alejandro Mendez <amendez23@gmail.com>
2020-03-26 19:51:37 +0100

    Packaging improvements

--- setup.py
+++ setup.py
@@ -1,44 +1,47 @@
 import io
-from setuptools import setup
+import re
+from setuptools import setup, find_packages
 
-from webvtt import __version__
+with io.open('README.rst', 'r', encoding='utf-8') as f:
+    readme = f.read()
 
+with io.open('webvtt/__init__.py', 'rt', encoding='utf-8') as f:
+    version = re.search(r'__version__ = \'(.*?)\'', f.read()).group(1)
 
-def readme():
-    with io.open('README.rst', 'r', encoding='utf-8') as f:
-        return f.read()
 
 setup(
     name='webvtt-py',
-    version=__version__,
+    version=version,
     description='WebVTT reader, writer and segmenter',
-    long_description=readme(),
+    long_description=readme,
     author='Alejandro Mendez',
     author_email='amendez23@gmail.com',
     url='https://github.com/glut23/webvtt-py',
-    packages=[
-        'webvtt',
-    ],
+    packages=find_packages('.', exclude=['tests']),
     include_package_data=True,
-    install_requires=['docopt'],
+    install_requires=[
+        'docopt'
+    ],
     entry_points={
         'console_scripts': [
             'webvtt=webvtt.cli:main'
         ]
     },
     license='MIT',
+    python_requires='>=3.4',
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
         'Operating System :: OS Independent',
         'Topic :: Software Development :: Libraries :: Python Modules',
     ],
     keywords='webvtt captions',
-    test_suite='tests'
 )
--- tests/test_webvtt_parser.py
+++ tests/test_webvtt_parser.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 from .generic import GenericParserTestCase
 
 import webvtt
